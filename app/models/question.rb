4899222b588819c966feffdd915656f4ce71fc84
risa5 <rir31677@gmail.com>
2025-07-11 17:22:56 +0900

    upadate:seedデータ投入①

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   validates :fatigue_category, presence: true, length: { maximum: 255 }
   validates :question_content, presence: true, length: { maximum: 255 }
-  enum fatigue_category: { body: 0, emotion: 1, mind: 2 }
+  enum fatigue_category: { body: 1, emotion: 2, mind: 3 }
   
   has_many :diagnosis_answers
 end
