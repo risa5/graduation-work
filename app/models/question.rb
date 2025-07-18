5b35d7eda1329228b9a9f7e2c678adc00477af61
risa5 <rir31677@gmail.com>
2025-07-19 03:45:36 +0900

    fix：Questionmodel

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,6 @@ class Question < ApplicationRecord
   validates :fatigue_category, presence: true, length: { maximum: 255 }
   validates :question_content, presence: true, length: { maximum: 255 }
   enum fatigue_category: { body: 1, emotion: 2, mind: 3 }
-  
-  belongs_to :diagnosis_result, optional: true
+
   has_many :choices
 end
