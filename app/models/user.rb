fff7ec0fb8d00ad82cffbcb32664d3c29f66851b
risa5 <rir31677@gmail.com>
2025-10-04 01:29:56 +0900

    update:profile_image_display

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
-  validates :image, length: { maximum: 255 }, allow_blank: true
 
   has_many :boards, dependent: :destroy
   has_many :diagnosis_records
