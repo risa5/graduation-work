2142e4994df4fc06157883b2443629181233237d
risa5 <rir31677@gmail.com>
2025-10-07 23:48:37 +0900

    migrate:SorceryResetPassword

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   get    "login",  to: "user_sessions#new"
   post   "login",  to: "user_sessions#create"
   delete "logout", to: "user_sessions#destroy", as: :logout
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  
 end
