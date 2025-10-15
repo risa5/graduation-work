519174454dde34946e845b56876f972c44671f27
risa5 <rir31677@gmail.com>
2025-10-15 23:16:11 +0900

    add:controller

---
+++ app/controllers/admin/boards_controller.rb
@@ -0,0 +1,8 @@
+class Admin::BoardsController < BaseController
+  before_action :require_admin!
+
+  def index
+    @q = Board.ransack(params[:q])
+    @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+  end
+end
