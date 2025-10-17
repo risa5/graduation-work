98e0d666f66f18064ebdeb6ed5f4fe7c097d6c32
risa5 <rir31677@gmail.com>
2025-10-17 22:50:24 +0900

    add:destroy

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -6,6 +6,13 @@ class Admin::BoardsController < ApplicationController
     @boards = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
+  
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy!
+    redirect_to admin_boards_path, success: '削除に成功しました', status: :see_other
+  end
+
   private
 
   def board_params
