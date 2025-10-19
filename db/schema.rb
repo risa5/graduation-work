efffa0446dcfb570a25b25b438da7996d464728e
risa5 <rir31677@gmail.com>
2025-10-19 22:46:19 +0900

    migrate:users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_13_130709) do
+ActiveRecord::Schema[7.2].define(version: 2025_10_19_134456) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
@@ -120,6 +120,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_13_130709) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.integer "role", default: 0, null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
