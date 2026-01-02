e91446337a109937768f7e507aac46e3854dbe20
risa5 <rir31677@gmail.com>
2026-01-02 21:50:57 +0900

    add:terms&policy_message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :require_login, only: %i[terms]
+
+  # 表示するための空のアクション
+  def terms; end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,7 @@
 class StaticPagesController < ApplicationController
-  # `require_login` をスキップし、未ログインユーザーでも `top` アクションにアクセスできるようにする
   skip_before_action :require_login, only: %i[top]
 
-  # トップページ（ルートページ）の表示アクション
+  # トップページの表示アクション
   def top; end
-  # 空のメソッド（`top.html.erb` のビューを表示するだけ）
 end
 
