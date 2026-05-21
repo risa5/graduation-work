f519934748e7507e76c853d7c74baea0f9d32910
risa5 <rir31677@gmail.com>
2026-05-21 21:09:38 +0900

    add:chart.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 import "bootstrap";
+import "chart.js";
+import {
+  Chart,
+  RadarController,
+  LineElement,
+  PointElement,
+  RadialLinearScale
+} from 'chart.js'
+
+Chart.register(
+  RadarController,
+  LineElement,
+  PointElement,
+  RadialLinearScale
+)
